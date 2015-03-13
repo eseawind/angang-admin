fd2be18d855760c74ea0bc22a97c21b7dd731ed4
quinnzhuang <mail.zhuangkun@gmail.com>
2015-03-13 10:53:28 +0800

    测试代码增加事务管理

--- src/main/java/org/plateau/angang/controllers/DemoController.java
+++ src/main/java/org/plateau/angang/controllers/DemoController.java
@@ -1,6 +1,6 @@
 package org.plateau.angang.controllers;
 
-import org.plateau.angang.dao.DemoMapper;
+import org.plateau.angang.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/demo")
 public class DemoController {
-	
+
 	@Autowired
-	private DemoMapper mapper;
-	
+	private DemoService service;
+
 	@RequestMapping("/index")
 	public String demo() {
-		System.out.println("result is " + mapper.getDemo(1));
+		System.out.println("result is " + service.getDemoById(1));
 		return "I'm demo!";
 	}
 
---
+++ src/main/java/org/plateau/angang/service/DemoService.java
@@ -0,0 +1,20 @@
+package org.plateau.angang.service;
+
+import org.plateau.angang.dao.DemoMapper;
+import org.plateau.angang.models.Demo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class DemoService {
+	
+	@Autowired
+	private DemoMapper mapper;
+	
+	public Demo getDemoById(int id) {
+		return mapper.getDemo(id);
+	}
+
+}
